03be89ba1616b986b9ceae27fc546248d5db27fa
Brenda Wicker <bwicker@byui.edu>
2020-09-26 13:49:30 -0500

    Week 2 updates

---
+++ js/main.js
@@ -0,0 +1,20 @@
+<script>
+
+/* dynamically created links for index page */
+const links = [
+  {label: 'Week 1 Notes', url: 'week01_notes.html', },
+  {label: 'Week 2 Notes', url: 'week02_notes.html', },
+
+];
+
+const ol = document.getElementById('assignments');  //use ol tag id
+links.ForEach(link => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', link.url);
+  a.innerText = link.label;
+  li.appendChild(a);
+  ol.appendChild(li);
+});
+
+</script>
